5e3ba91192191cfe5aa81a5979ac9b9eb2db4a57
mgeier82 <mgeier82@gmail.com>
2016-06-15 11:05:45 +0200

    deploy

--- modules/opportunity.js
+++ modules/opportunity.js
@@ -4,11 +4,6 @@ var org = require('./auth').org,
 
 function execute(req, res) {
 
-    res.send(req.body.token + " " + OPPORTUNITY_TOKEN);
-    return;
-
-    
-
     if (req.body.token != OPPORTUNITY_TOKEN) {
          res.send("Invalid token");
          return;
